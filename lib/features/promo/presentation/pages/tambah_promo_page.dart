fc6602681561f6ce9fcf8a8f02888386d0936bfd
Zuuru <zyzull09@gmail.com>
2026-06-17 08:21:51 +0700

    Update tambah promo page

--- lib/features/promo/presentation/pages/tambah_promo_page.dart
+++ lib/features/promo/presentation/pages/tambah_promo_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'dart:io';
 import '../../data/promo_model.dart';
 import '../../data/promo_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -38,6 +38,7 @@ class _TambahPromoPageState extends State<TambahPromoPage> {
   DateTime? _mulai;
   DateTime? _berakhir;
   bool _isLoading = false;
+  File? _imageFile;
 
   bool get _isEditMode => widget.existingPromo != null;
 
@@ -129,7 +130,7 @@ class _TambahPromoPageState extends State<TambahPromoPage> {
     setState(() => _isLoading = true);
 
     try {
-      final String? finalImageUrl = _imageUrlController.text.trim().isNotEmpty
+      String? finalImageUrl = _imageUrlController.text.trim().isNotEmpty
           ? _imageUrlController.text.trim()
           : null;
       if (_imageFile != null) {
